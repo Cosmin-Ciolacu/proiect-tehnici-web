31bd03ec87c3f76fe6380a671846387e9a332166
Cosmin Ciolacu <cosmin.ciolacu@dev51.io>
2020-12-31 16:08:23 +0200

    finish color picker

--- pages/color-picker/main.js
+++ pages/color-picker/main.js
@@ -1,9 +1,35 @@
 const canvas = document.querySelector("#colors");
 const ctx = canvas.getContext("2d");
+const rgbColorSpan = document.querySelector("#rgbColor");
+const hexColorSpan = document.querySelector("#hexColor");
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
 
-const image = new Image(250, 250);
-image.onload = () => ctx.drawImage(image, 0, 0, image.width, image.height);
-image.src = "./assets/colors.png";
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+function selectColor(rgb = null) {
+  if (rgb !== null) {
+    const rgbColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+    const hexColor = rgbToHex(rgb[0], rgb[1], rgb[2]);
+    document.body.style.backgroundColor = hexColor;
+    rgbColorSpan.innerHTML = rgbColor;
+    hexColorSpan.innerHTML = hexColor;
+  } else {
+    rgbColorSpan.innerHTML = "rgb(255, 255, 255)";
+    hexColorSpan.innerHTML = "#ffffff";
+  }
+}
+
+function initImage() {
+  const image = new Image(250, 250);
+  image.onload = () => ctx.drawImage(image, 0, 0, image.width, image.height);
+  image.src = "./assets/colors.png";
+  selectColor();
+}
+window.onload = initImage;
 
 canvas.onclick = (mouseEvent) => {
   const imgData = ctx.getImageData(
@@ -12,6 +38,6 @@ canvas.onclick = (mouseEvent) => {
     1,
     1
   );
-  const rgba = imgData.data;
-  console.log(rgba);
+  const rgb = imgData.data;
+  selectColor(rgb);
 };
